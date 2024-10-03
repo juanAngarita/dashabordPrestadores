b8f1e760506b13a600544ba9d8291291279434ad
juanAngarita <angaritat.js@javeriana.edu.co>
2024-10-03 18:24:41 -0500

    solucionando problemas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: DashboardComponent }];
